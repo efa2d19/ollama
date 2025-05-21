97f808417a6ef8f842e9cc39efa77733bd24f961
efa2d19 <44712637+efa2d19@users.noreply.github.com>
2025-05-21 20:12:08 +0400

    ci: truncated && removed unused workflows

--- scripts/build_darwin.sh
+++ scripts/build_darwin.sh
@@ -12,7 +12,7 @@
 #    VOL_NAME="$(date)" ./scripts/build_darwin.sh
 #
 VOL_NAME=${VOL_NAME:-"Ollama"}
-export VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --long --dirty --always | sed -e "s/^v//g")}
+export VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --long --dirty --always)}
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=${VERSION#v}\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 export CGO_CFLAGS="-mmacosx-version-min=14.0"
 export CGO_CXXFLAGS="-mmacosx-version-min=14.0"
@@ -29,7 +29,7 @@ usage() {
 mkdir -p dist
 
 
-ARCHS="arm64 amd64"
+ARCHS="arm64"
 while getopts "a:h" OPTION; do
     case $OPTION in
         a) ARCHS=$OPTARG ;;
@@ -44,16 +44,6 @@ _build_darwin() {
         status "Building darwin $ARCH"
         INSTALL_PREFIX=dist/darwin-$ARCH/
         GOOS=darwin GOARCH=$ARCH CGO_ENABLED=1 go build -o $INSTALL_PREFIX .
-
-        if [ "$ARCH" = "amd64" ]; then
-            status "Building darwin $ARCH dynamic backends"
-            cmake -B build/darwin-$ARCH \
-                -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-                -DCMAKE_OSX_DEPLOYMENT_TARGET=11.3 \
-                -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-            cmake --build build/darwin-$ARCH --target ggml-cpu -j
-            cmake --install build/darwin-$ARCH --component CPU
-        fi
     done
 }
 
@@ -64,7 +54,7 @@ _sign_darwin() {
     chmod +x dist/darwin/ollama
 
     if [ -n "$APPLE_IDENTITY" ]; then
-        for F in dist/darwin/ollama dist/darwin-amd64/lib/ollama/*; do
+        for F in dist/darwin/ollama; do
             codesign -f --timestamp -s "$APPLE_IDENTITY" --identifier ai.ollama.ollama --options=runtime $F
         done
 
@@ -77,7 +67,6 @@ _sign_darwin() {
 
     status "Creating universal tarball..."
     tar -cf dist/ollama-darwin.tar --strip-components 2 dist/darwin/ollama
-    tar -rf dist/ollama-darwin.tar --strip-components 4 dist/darwin-amd64/lib/
     gzip -9vc <dist/ollama-darwin.tar >dist/ollama-darwin.tgz
 }
 
@@ -108,11 +97,10 @@ _build_macapp() {
     touch dist/Ollama.app
 
     go clean -cache
-    GOARCH=amd64 CGO_ENABLED=1 GOOS=darwin go build -o dist/darwin-app-amd64 -ldflags="-s -w -X=github.com/ollama/ollama/app/version.Version=${VERSION}" ./app/cmd/app
     GOARCH=arm64 CGO_ENABLED=1 GOOS=darwin go build -o dist/darwin-app-arm64 -ldflags="-s -w -X=github.com/ollama/ollama/app/version.Version=${VERSION}" ./app/cmd/app
     mkdir -p dist/Ollama.app/Contents/MacOS
-    lipo -create -output dist/Ollama.app/Contents/MacOS/Ollama dist/darwin-app-amd64 dist/darwin-app-arm64
-    rm -f dist/darwin-app-amd64 dist/darwin-app-arm64
+    lipo -create -output dist/Ollama.app/Contents/MacOS/Ollama dist/darwin-app-arm64
+    rm -f dist/darwin-app-arm64
 
     # Create a mock Squirrel.framework bundle
     mkdir -p dist/Ollama.app/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/
@@ -129,13 +117,8 @@ _build_macapp() {
 
     # Setup the ollama binaries
     mkdir -p dist/Ollama.app/Contents/Resources
-    if [ -d dist/darwin-amd64 ]; then
-        lipo -create -output dist/Ollama.app/Contents/Resources/ollama dist/darwin-amd64/ollama dist/darwin-arm64/ollama
-        cp dist/darwin-amd64/lib/ollama/*.so dist/darwin-amd64/lib/ollama/*.dylib dist/Ollama.app/Contents/Resources/
-    else
-        cp -a dist/darwin/ollama dist/Ollama.app/Contents/Resources/ollama
-        cp dist/darwin/*.so dist/darwin/*.dylib dist/Ollama.app/Contents/Resources/
-    fi
+    cp -a dist/darwin/ollama dist/Ollama.app/Contents/Resources/ollama
+    cp dist/darwin/*.so dist/darwin/*.dylib dist/Ollama.app/Contents/Resources/
     chmod a+x dist/Ollama.app/Contents/Resources/ollama
 
     # Sign
@@ -187,7 +170,6 @@ _build_macapp() {
 if [ "$#" -eq 0 ]; then
     _build_darwin
     _sign_darwin
-    _build_macapp
     exit 0
 fi
 
